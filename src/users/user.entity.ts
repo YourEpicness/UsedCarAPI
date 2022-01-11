eb856fcfcf2a7d1c2a3dfd918266964ce82c7acf
Julaam Diop <jeebe.diop@gmail.com>
2022-01-11 00:12:56 -0500

    Add CRUD logic for users

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import {
+  AfterInsert,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterRemove,
+  AfterUpdate,
+} from "typeorm";
 
 // Step 1: Create an entity that represents the data types
 @Entity()
@@ -11,4 +18,20 @@ export class User {
 
   @Column()
   password: string;
+
+  // Hooks: Only executed on entity instance (create) NOT directly passing {}
+  @AfterInsert()
+  logInsert() {
+    console.log("Inserted User with id", this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log("Removed User with id", this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log("Updated User with id", this.id);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,9 +7,40 @@ import { User } from "./user.entity";
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
+  // Service functions will talk to repository which executes function
   create(email: string, password: string) {
-    const user = this.repo.create({ email, password });
+    // Creates a user entity in the user.entity file
+    // USeful for validation
+    const user = this.repo.create({ email, password }); // CAN USE HOOKS
 
     return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOne(id);
+  }
+
+  // Returns an array with criteria met
+  // Returns empty array if not found
+  find(email: string) {
+    return this.repo.find({ email });
+  }
+
+  // Partial<w/e> can use part of any type
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    return this.repo.remove(user);
   }
 }
