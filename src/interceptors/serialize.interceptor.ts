89a8bc03a03bd5733b6e9531e225df2debc15a2a
Julaam Diop <jeebe.diop@gmail.com>
2022-01-11 22:19:14 -0500

    Create a decorator to simplify serialize interceptor

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -9,6 +9,10 @@ import { map } from "rxjs";
 import { plainToInstance } from "class-transformer";
 import { UserDto } from "../users/dtos/user.dto";
 
+export function Serialize(dto: any) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
 export class SerializeInterceptor implements NestInterceptor {
   constructor(private dto: any) {}
 
