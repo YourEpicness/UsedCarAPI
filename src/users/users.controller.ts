5d5ef5fe2ef725c0829ae1fb2d2739fc9546368f
Julaam Diop <jeebe.diop@gmail.com>
2022-01-12 01:18:04 -0500

    Add globally scoped current-user interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,8 +9,8 @@ import {
   Delete,
   NotFoundException,
   Session,
-  UseInterceptors,
 } from "@nestjs/common";
+
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { UpdateUserDto } from "./dtos/update-user.dto";
 import { UsersService } from "./users.service";
@@ -18,12 +18,10 @@ import { UserDto } from "./dtos/user.dto";
 import { Serialize } from "../interceptors/serialize.interceptor";
 import { AuthService } from "./auth.service";
 import { CurrentUser } from "./decorators/current-user.decorator";
-import { CurrentUserInterceptor } from "./interceptors/current-user.interceptor";
 import { User } from "./user.entity";
 
 @Controller("auth")
 @Serialize(UserDto)
-@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService,
