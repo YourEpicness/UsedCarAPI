100fe64c1c328a5f6c9af7484d1ace858f46cea3
Julaam Diop <jeebe.diop@gmail.com>
2022-01-11 22:14:01 -0500

    Refactor to use any dto with interceptor

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -6,10 +6,12 @@ import {
 } from "@nestjs/common";
 import { Observable } from "rxjs";
 import { map } from "rxjs";
-import { plainToClass } from "class-transformer";
+import { plainToInstance } from "class-transformer";
 import { UserDto } from "../users/dtos/user.dto";
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     // Run something before a request is handled by the request handler
     return handler.handle().pipe(
@@ -17,7 +19,7 @@ export class SerializeInterceptor implements NestInterceptor {
         // Run something before the response is sent out
 
         // Convert data to an instance of userDTO
-        return plainToClass(UserDto, data, {
+        return plainToInstance(this.dto, data, {
           // Removes the properties that aren't Expose in DTO
           excludeExtraneousValues: true,
         });
